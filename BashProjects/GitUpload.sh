f65ea3581a6da809a8c31305a26691ad09eb0742
Scorpion <scorupi819@outlook.com>
2021-01-08 23:20:09 -0700

    testing commit script

--- BashProjects/GitUpload.sh
+++ BashProjects/GitUpload.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
+
 cd
 cd ProgrammingProjects
 
 eval 'ssh-agent -s'
 
-Cuda='date -u'
+Cuda=date+"%A, %b %d, %Y %I: %M %p"
 echo $Cuda
+
+cat $Cuda >> README.md
 
 git init
 git add ArduinoProjects/
@@ -15,7 +19,7 @@ git add PythonProjects/
 git add BashProjects/
 git add CProjects/
 git add PolyProjects/
-git commit -m $Cuda
+git commit -m "Testing Commit Script"
 git branch -M main
 git remote add set-url origin git@github.com:ScorpionKing819/ProgrammingProjects.git
 git push --set-upstream origin main
