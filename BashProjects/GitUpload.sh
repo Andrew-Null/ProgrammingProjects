d35ea3a64576e82aabe631b0c98e791d1104240d
Scorpion <scorupi819@outlook.com>
2021-01-08 21:42:04 -0700

    testing commit script

--- BashProjects/GitUpload.sh
+++ BashProjects/GitUpload.sh
@@ -2,6 +2,8 @@ cd
 cd ProgrammingProjects
 CuDa='date'
 
+eval 'ssh-gent -s'
+
 git init
 git add ArduinoProjects/
 git add CPProjects/
@@ -13,5 +15,5 @@ git add CProjects/
 git add PolyProjects/
 git commit -m $CuDa
 git branch -M main
-git remote add origin https://github.com/ScorpionKing819/ProgrammingProjects.git
+git remote add origin git@github.com:ScorpionKing819/ProgrammingProjects.git
 git push --force origin main
